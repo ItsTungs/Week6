091511473c91c186ea163fc1dfae0b1d63d1dc2e
ItsTungs <105867103+ItsTungs@users.noreply.github.com>
2023-04-10 11:47:22 +0700

    Add files via upload

---
+++ Week6/linkcookies.php
@@ -0,0 +1,4 @@
+<?php
+setcookie("variable_cookies","ini adalah varaiable cookies", time()+60);
+echo "<a href=cekcookies.php>Cek Cookies</a>"
+?>
---
+++ Week6/mod_2.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title> Pemrograman PHP dengan Arrays</title>
+    </head>
+    <body>
+        <?php
+        $nama[] = "Rayhan Favian";
+        $nama[] = "Al";
+        $nama[] = "Gaffar";
+        echo $nama[1] . $nama[2] . $nama[0];
+        echo "<br>";
+        ?>
+    </body>
+</html>
